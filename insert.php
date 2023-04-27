fb124e9724990c4d877b9b0aeb706859f06e70b7
Ryan Nicolau Lopes <61668167+ryannlopes@users.noreply.github.com>
2023-04-27 20:32:35 -0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -6,13 +6,25 @@
     $link = mysqli_connect($host, $user, $pass, $bd);
 
     //Dados
-    $nomeEstado = "Sao Paulo";
+    $nomeEstado = "Bahia";
 
-    $query = "INSERT INTO estado(nomeEstado) VALUES ('$nomeEstado')";
+    $query = "INSERT INTO estado(idEstado, nomeEstado) VALUES (default, '$nomeEstado')";
+
+
     if(mysqli_query($link, $query)){
         echo "Criado com sucesso";
     }else{
-        echo "Usuario não Criado"
+        echo "Usuario não Criado";
     }
+        
+    $query = "SELECT * FROM estado";
+                  $busca = mysqli_query($link, $query);
+
+                  while ($dados = mysqli_fetch_array($busca)) {
+                    $id = $dados['idEstado'];
+                    $nome = $dados['nomeEstado'];
+
+                    echo $id . $nome;
+                  }
     mysqli_close($link);
 ?>
